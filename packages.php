b244c7a0320e2e565a31ba9bda9d9a5cac76cae4
the-siegmeister <siegmeister@protonmail.com>
2021-02-11 15:26:55 +0800

    Create helpers for identifying user type of authenticated user Add buttons for package functionality which do nothing for now

--- packages.php
+++ packages.php
@@ -6,6 +6,7 @@
   <?php
     // query tall
     include('./db/connection.php');
+    include('./actions/helpers.php');
     $query = "select * from packages";
     $results = mysqli_query($db_conn, $query);
   ?>
@@ -13,23 +14,33 @@
   <?php if (mysqli_num_rows($results) != 0) { ?>
     <div class='row m-auto w-75 p-4'>
       <?php foreach($results as $package) { ?>
-        <a class="custom-link col-4 text-decoration-none text-dark mb-4"
-          href="/package/index.php?package=<?php echo htmlspecialchars($package['id']); ?>">
+        <div class='col-4 mb-4'>
           <div class="card shadow bg-light rounded">
-            <div class="card-body">
-              <h5 class="card-title">
-                <?php echo htmlspecialchars($package['name']); ?>
-              </h5>
-              <p class='card-text'>
-                <span class='fw-bold'> Price: Php</span>
-                <?php echo htmlspecialchars($package['price']) ?>
-              </p>
-              <p class="card-text">
-                <?php echo "*" . htmlspecialchars($package['perks']); ?>
-              </p>
-            </div>
-          </div>
-        </a>
+            <a class="text-decoration-none text-dark"
+              href="/package/index.php?package=<?php echo htmlspecialchars($package['id']); ?>">
+              <div class="card-body">
+                <h5 class="card-title">
+                  <?php echo htmlspecialchars($package['name']); ?>
+                </h5>
+                <p class='card-text'>
+                  <span class='fw-bold'> Price: Php</span>
+                  <?php echo htmlspecialchars($package['price']) ?>
+                </p>
+                <p class="card-text">
+                  <?php echo "*" . htmlspecialchars($package['perks']); ?>
+                </p>
+              </div>
+            </a>
+
+            <?php if (isAdmin()) { ?>
+              <section class='actions p-2'>
+                <button class='btn btn-sm btn-success'>edit</button>
+                <button class='btn btn-sm btn-danger'>delete</button>
+              </section>
+            <?php } ?>
+
+          </div> <!-- card -->
+        </div> <!-- col -->
       <?php } ?>
     </div>
   <?php } else { ?>
